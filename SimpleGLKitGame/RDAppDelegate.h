125ae32264c5570ecf5da65ac92360c58de7f762
Zakk Hoyt <zakk@threefoldphotos.com>
2014-04-16 10:52:13 -0700

    Initial Commit

---
+++ SimpleGLKitGame/RDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RDAppDelegate.h
+//  SimpleGLKitGame
+//
+//  Created by Zakk Hoyt on 4/16/14.
+//  Copyright (c) 2014 Zakk Hoyt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
